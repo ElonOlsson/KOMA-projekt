1319e4a4c0862e73ff8018b238b64997ea3059cc
trainlock <trainlock@gmail.com>
2016-11-08 08:23:02 +0100

    Nytt kodskelett som är lättare att arbeta med

---
+++ Koood/app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.home' //<--- Här lägger ni till era moduler se home.js
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+
+  $routeProvider.otherwise({redirectTo: '/home'}); //<--- Här skriver ni er startsida
+}]);
