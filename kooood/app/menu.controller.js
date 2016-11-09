15df1e819ca6bde62ccf97c022eee135846cd52a
ElonOlsson <olssonelon@gmail.com>
2016-11-09 10:37:20 +0100

    jag tog bort allt mitt lokala för att hämta det nya skelettet

--- kooood/app/menu.controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('myApp.menu', []);
-
-angular.module('myApp.menu').controller('menuCtrl', ['$scope', '$location', function($scope, $location){
-	$scope.isActive = function(path) {
-		 return $location.path() == path;
-	}
-}]);
